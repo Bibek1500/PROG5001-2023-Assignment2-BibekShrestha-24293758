45c81ff870d997d9214c05052cea2ed7998df559
Bibek1500 <80021162+Bibek1500@users.noreply.github.com>
2023-10-02 22:35:29 +1100

    Top 5 student with highest and lowest marks
    All errors fixed.

--- Main.java
+++ Main.java
@@ -19,6 +19,8 @@ public class Main
         System.out.print("Enter the path to the CSV file: ");
         String fileName = scanner.nextLine();
         try {
+            // F1: Read data from file 
+            
             ArrayList<Student> students = StudentReader.readCSV(fileName);
 
             System.out.println("Please select a number: ");
@@ -46,11 +48,13 @@ public class Main
                 }
                 
                 if(userEntry == 3) {
-                
+                    System.out.println("List of 5 students with highest marks");
+                    topFiveStudents(students);
                 }
                 
                 if(userEntry == 4) {
-                
+                    System.out.println("List of 5 students with lowest marks");
+                    topFiveLowestStudents(students);
                 }
                 
                 System.out.println("Please select a number: ");
@@ -73,6 +77,7 @@ public class Main
         }
     }
     
+    //F2: Print student total marks
     public static void printStudentTotal(ArrayList<Student> students) {
         for (Student student : students) {
                 System.out.println("firstName: " + student.getfirstName() +
@@ -86,6 +91,7 @@ public class Main
          System.out.println();   
     }
     
+    //F3: Print list of students with total marks less than threshold
     public static void printStudentTotalLessThanThreshold(ArrayList<Student> students, double threshold) {
         for (Student student : students) {
                 if(student.calculateTotal() < threshold) {
@@ -99,5 +105,75 @@ public class Main
                 }
         }
          System.out.println();   
+    }
+    
+    //F4: Top 5 students with highest marks
+    public static void topFiveStudents(ArrayList<Student> students) {
+        ArrayList<Student> topFiveStudents = new ArrayList<>();
+        
+        for (Student student : students) {
+            if (topFiveStudents.size() < 5) {
+                topFiveStudents.add(student);
+            } else {
+                // Find the student with the lowest total marks in the current top 5
+                Student lowestTotalStudent = topFiveStudents.get(0);
+                for (Student topStudent : topFiveStudents) {
+                    if (topStudent.calculateTotal() < lowestTotalStudent.calculateTotal()) {
+                        lowestTotalStudent = topStudent;
+                    }
+                }
+
+                // If the current student has a higher total, replace the lowest total student
+                if (student.calculateTotal() > lowestTotalStudent.calculateTotal()) {
+                    topFiveStudents.remove(lowestTotalStudent);
+                    topFiveStudents.add(student);
+                }
+            }
+        }
+            
+            for (Student student : topFiveStudents) {
+                System.out.println("firstName: " + student.getfirstName() +
+                       ", lastName: " + student.getlastName() +
+                       ", studentId: " + student.getstudentId() +
+                       ", a1: " + student.geta1() +
+                       ", a2: " + student.geta2() +
+                       ", a3: " + student.geta3() +
+                       ", total: " + student.calculateTotal());
+        }  
+    }
+    
+    //F5: Top 5 students with lowest marks
+    public static void topFiveLowestStudents(ArrayList<Student> students) {
+        ArrayList<Student> topFiveLowestStudents = new ArrayList<>();
+        
+        for (Student student : students) {
+            if (topFiveLowestStudents.size() < 5) {
+                topFiveLowestStudents.add(student);
+            } else {
+                // Find the student with the higest total marks in the current top 5
+                Student highestTotalStudent = topFiveLowestStudents.get(0);
+                for (Student lowestStudent : topFiveLowestStudents) {
+                    if (lowestStudent.calculateTotal() > highestTotalStudent.calculateTotal()) {
+                        highestTotalStudent = lowestStudent;
+                    }
+                }
+
+                // If the current student has a lowest total, replace the highest total student
+                if (student.calculateTotal() < highestTotalStudent.calculateTotal()) {
+                    topFiveLowestStudents.remove(highestTotalStudent);
+                    topFiveLowestStudents.add(student);
+                }
+            }
+        }
+            
+            for (Student student : topFiveLowestStudents) {
+                System.out.println("firstName: " + student.getfirstName() +
+                       ", lastName: " + student.getlastName() +
+                       ", studentId: " + student.getstudentId() +
+                       ", a1: " + student.geta1() +
+                       ", a2: " + student.geta2() +
+                       ", a3: " + student.geta3() +
+                       ", total: " + student.calculateTotal());
+        }  
     }
 }
